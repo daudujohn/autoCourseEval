0b4ecc5fa301f38e5aac0e2dabcc04c13cc0a46d
daudujohn <daudujoe@gmail.com>
2022-11-04 15:50:18 +0100

    Initial commit

--- fill_eval.py
+++ fill_eval.py
@@ -4,6 +4,16 @@ from selenium.webdriver.common.by import By
 import time
 
 def fill_eval(driver, url, elements, submit = False, lecturer = False):
+    """
+    It takes a list of elements, and fills them into the evaluation form
+    
+    :param driver: The webdriver object
+    :param url: The url of the evaluation page
+    :param elements: A list of the answers to the questions
+    :param submit: If True, the evaluation will be submitted, defaults to False (optional)
+    :param lecturer: If you want to enter the lecturer for the week, set this to True, defaults to False
+    (optional)
+    """
     driver.get(url)
     
 
@@ -13,6 +23,7 @@ def fill_eval(driver, url, elements, submit = False, lecturer = False):
         driver.find_element(By.XPATH, '//*[starts-with(@id,"id_multichoice_")]').send_keys((Keys.TAB * i), elements[i])
 
     if submit:
-        time.sleep(3)
+        time.sleep(2)
         driver.find_element(By.LINK_TEXT, "Submit your answers").click()
+        print("Evaluation Submitted.")
 
