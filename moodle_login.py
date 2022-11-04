689735763d8890230c81bb2ead0a380417576e5f
daudujohn <daudujoe@gmail.com>
2022-11-04 15:50:35 +0100

    Initial commit

--- moodle_login.py
+++ moodle_login.py
@@ -2,10 +2,6 @@ from selenium.webdriver.common.by import By
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-
 def moodle_login(driver):
     """
     It finds the username and password fields, clears them, enters the username and password, and clicks
@@ -13,6 +9,11 @@ def moodle_login(driver):
     
     :param driver: the webdriver object
     """
+    load_dotenv()
+
+    USERNAME = os.environ.get('user_name')
+    PASSWORD = os.environ.get('pass_word')
+
     username = driver.find_element(By.ID, "username")
     password = driver.find_element(By.ID, "password")
     username.clear()
